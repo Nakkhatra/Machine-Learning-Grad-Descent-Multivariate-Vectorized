0ea207b495cfa40602ac7726766cdbc5ceb83ab9
Nakkhatra <64383902+Nakkhatra@users.noreply.github.com>
2020-04-28 00:06:40 +0600

    Update testingforquiz.m

--- testingforquiz.m
+++ testingforquiz.m
@@ -1,3 +1,5 @@
+%Assumption of values for X,y,theta,num_iters and alpha
+
 X=[1 1 3;1 3 2;1 2 5;1 4 1;1 1 6];
 y=[7;9;15;11;12];
 theta=[0;50;5.5]
@@ -5,7 +7,7 @@ num_iters=100;
 alpha=0.01;
 
 
-
+%main code
 
 n=size(X,2)-1;
 m = length(y); 
